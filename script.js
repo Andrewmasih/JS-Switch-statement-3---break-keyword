6ff9586d0ef96b54e11eef5c6bffabcb20f0b2ce
Andrew Masih <armasih@outlook.com>
2021-05-08 21:22:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let fruit = "Banana";
+
+switch (fruit) {
+  case "Apple": console.log("the first fruit ia a apple");
+    break;
+
+  case "Lemon": console.log("the fruit is a lemon");
+    break;
+
+  case "Strawberry": console.log("the fruit is a strawberry");
+    break;
+
+  default: console.log("this is the default value when none of the cases were TRUE");
+
+  /* The BREAK keyword indicates the end of the CASE, if the BREAK is not there JS does not stop and moves on to the next statement. */
+
+}
